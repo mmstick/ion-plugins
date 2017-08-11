c0ac88adeac914f9fcd308f93d38299a2cc11927
Sehny <msehnout@users.noreply.github.com>
2017-08-11 17:47:02 +0200

    Add functions to count staged and modified files.
    Can be used for example like this:
    let PROMPT = "${PWD} (${git::branch}|●${git::staged_count}|+${git::modified_count}) \$ "

--- src/namespaces/strings/git/lib.rs
+++ src/namespaces/strings/git/lib.rs
@@ -16,7 +16,7 @@ pub struct NamespaceResult {
 // Provides a space-delimited list of symbols provided by this library.
 #[no_mangle]
 pub extern "C" fn index() -> *const u8 {
-    b"branch\0".as_ptr()
+    b"branch modified_count staged_count\0".as_ptr()
 }
 
 
@@ -38,4 +38,57 @@ pub extern "C" fn branch() -> NamespaceResult {
         exists: false,
         data: unsafe { uninitialized() },
     }
+}
+
+// Iterates over statuses of all files in a repository
+fn iterate_statuses<F>(mut iterator_fn: F)
+    where F: FnMut(&git2::StatusEntry) {
+    if let Ok(dir) = current_dir() {
+        if let Ok(repo) = Repository::open(dir) {
+            if let Ok(statuses) = repo.statuses(None) {
+                for i in statuses.iter() {
+                    iterator_fn(&i);
+                }
+            }
+        }
+    }
+}
+
+// Build NamespaceResult from a String
+fn build_return_value(s: String) -> NamespaceResult {
+    let string = CString::new(s.as_bytes()).unwrap();
+    let data = string.into_raw();
+    return NamespaceResult { exists: true, data };
+}
+
+// There is a common theme among functions such as changed_count, staged_count, etc.
+// This function does the common part
+fn count_statuses(status: git2::Status) -> NamespaceResult {
+    let mut acc = 0;
+    iterate_statuses(|i| {
+        if i.status().intersects(status) {
+            acc += 1;
+        }
+    });
+
+    let ret_string: String = format!("{}", acc);
+    build_return_value(ret_string)
+}
+
+#[no_mangle]
+pub extern "C" fn modified_count() -> NamespaceResult {
+    use git2::*;
+
+    let modified = STATUS_WT_DELETED | STATUS_WT_MODIFIED | STATUS_WT_RENAMED | STATUS_WT_TYPECHANGE;
+    count_statuses(modified)
+}
+
+
+#[no_mangle]
+pub extern "C" fn staged_count() -> NamespaceResult {
+    use git2::*;
+
+    let staged =  STATUS_INDEX_DELETED | STATUS_INDEX_MODIFIED | STATUS_INDEX_NEW |
+                  STATUS_INDEX_RENAMED | STATUS_INDEX_TYPECHANGE;
+    count_statuses(staged)
 }
